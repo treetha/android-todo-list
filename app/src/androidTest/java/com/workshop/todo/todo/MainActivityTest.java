71d6023b73867020c3c8914f9ec69734e32a8d1f
somkiat <somkiat.p@gmail.com>
2017-02-27 20:58:47 +0700

    Snapshot with screengrab

--- app/src/androidTest/java/com/workshop/todo/todo/MainActivityTest.java
+++ app/src/androidTest/java/com/workshop/todo/todo/MainActivityTest.java
@@ -10,13 +10,20 @@ import com.workshop.todo.todo.interactor.TaskInteractor;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.IOException;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -29,13 +36,23 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 import static org.junit.Assert.*;
 
-
+@RunWith(JUnit4.class)
 public class MainActivityTest {
+
+    @ClassRule
+    public static final LocaleTestRule localeTestRule
+            = new LocaleTestRule();
+
     @Rule
     public ActivityTestRule activityTestRule
             = new ActivityTestRule(MainActivity.class, true, false);
 
     private MockWebServer server;
+
+    @BeforeClass
+    public static void beforeAll() {
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+    }
 
     @Before
     public void startMockWebServer() throws IOException {
@@ -67,17 +84,29 @@ public class MainActivityTest {
         Intent intent = new Intent();
         activityTestRule.launchActivity(intent);
 
+        Screengrab.screenshot("step_01");
+
         //Press signin button
         onView(withId(R.id.edt_user_name))
                 .perform(typeText("somkiat"), closeSoftKeyboard());
         onView(withId(R.id.edt_password))
                 .perform(typeText("pui"), closeSoftKeyboard());
+
+        Screengrab.screenshot("step_02");
+
         onView(withId(R.id.btn_login)).perform(click());
+
+        Screengrab.screenshot("step_03");
 
         //Check list of task
         onData(anything()).inAdapterView(withId(R.id.list_todo))
                 .atPosition(9).perform(click());
+
+        Screengrab.screenshot("step_04");
+
         onView(withId(R.id.task_description))
                 .check(matches(withText("Mock Webserver 10")));
+
+        Screengrab.screenshot("step_05");
     }
 }
