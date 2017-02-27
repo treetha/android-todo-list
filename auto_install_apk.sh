77e8e406394125e4643e36811a1c8dd7a5da0429
somkiat <somkiat.p@gmail.com>
2017-02-27 17:28:26 +0700

    Add shell script to install apk

---
+++ auto_install_apk.sh
@@ -0,0 +1,12 @@
+adb devices | while read line
+do
+    if [ ! "$line" = "" ] && [ `echo $line | awk '{print $2}'` = "device" ]
+    then
+        device=`echo $line | awk '{print $1}'`
+        echo "Run >>adb -s $device $@"
+        #adb -s $device uninstall com.workshop.todo.todo
+        adb -s $device $@
+    fi
+done
+
+#sh test.sh install -r app/build/outputs/apk/app-debug.apk
